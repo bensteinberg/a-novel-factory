0e35736be95ff7c5e10cc1351982265a4ed7fd30
bensteinberg <bsteinberg@law.harvard.edu>
2022-11-14 15:43:56 -0500

    Change import of random

--- a_novel_factory/factories.py
+++ a_novel_factory/factories.py
@@ -1,6 +1,6 @@
 import factory
+import random
 from a_novel_factory import objects, providers
-from random import randint, sample
 from faker import Faker
 
 fake = Faker()
@@ -37,7 +37,7 @@ class ParagraphFactory(factory.Factory):
         model = objects.Paragraph
 
     sentences = factory.LazyAttribute(
-        lambda o: SentenceFactory.create_batch(randint(2, 24),
+        lambda o: SentenceFactory.create_batch(random.randint(2, 24),
                                                characters=o.characters)
     )
 
@@ -49,11 +49,11 @@ class ChapterFactory(factory.Factory):
     number = factory.Sequence(lambda n: n + 1)
     title = factory.LazyAttribute(lambda _: TitleFactory.create())
     characters = factory.LazyAttribute(
-        lambda o: sample(o.novel_characters, k=randint(2, 4))
+        lambda o: random.sample(o.novel_characters, k=random.randint(2, 4))
     )
 
     paragraphs = factory.LazyAttribute(
-        lambda o: ParagraphFactory.create_batch(randint(10, 30),
+        lambda o: ParagraphFactory.create_batch(random.randint(10, 30),
                                                 characters=o.characters)
     )
 
@@ -63,6 +63,6 @@ class NovelFactory(factory.Factory):
         model = objects.Novel
 
     title = factory.SubFactory(TitleFactory)
-    characters = CharacterFactory.create_batch(randint(4, 18))
-    chapters = ChapterFactory.create_batch(randint(40, 50),
+    characters = CharacterFactory.create_batch(random.randint(4, 18))
+    chapters = ChapterFactory.create_batch(random.randint(40, 50),
                                            novel_characters=characters)
